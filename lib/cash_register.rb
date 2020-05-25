cc603ed56f53380a35b2866f486ad6b41b8d4a5c
Daniel Kwon <danielkwon89@gmail.com>
2020-05-25 14:34:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 class CashRegister
   
-  attr_accessor :total, :employee_discount
+  attr_accessor :total
+  # , :employee_discount
   
   def initialize(employee_discount = 0)
     @total = 0
-    @employee_discount = employee_discount
+    # @employee_discount = employee_discount
   end
   
 end
