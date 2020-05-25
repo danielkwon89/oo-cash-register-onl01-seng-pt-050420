b705d69ba3d6c4efaa104a6a246dfd18153d8119
Daniel Kwon <danielkwon89@gmail.com>
2020-05-25 14:39:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,10 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    @total = @total += price * quantity
+    @total += price * quantity
   end
   
   def apply_discount
-    
+    @total = @total * @discount
   end
 end
